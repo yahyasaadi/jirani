c7b94b9152394679af619b7f4b886f8ef087b6fb
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-20 11:20:37 +0300

    Created a profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField()
+    contact = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.width > 250 or img.height > 250:
+            memory_size = (250, 250)
+            img.thumbnail(memory_size)
+            img.save(self.image.path)
