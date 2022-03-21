fcf48080bc60852b5a1e6ac630ade5c00803ef7d
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-21 21:19:44 +0300

    Added the relevant urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('hood/new/', views.add_hood, name='create-hood'),
     path('hood/<int:id>/', views.hood_detail, name='hood-detail'),
     path('post/new/<int:hood_id>/', views.post, name='new-post'),
+    path('posts/<int:id>/', views.hood_posts, name='hood-posts'),
 ]
