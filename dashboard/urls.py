f533c91e3ed5ecfe781fd0b9206697f72f206e39
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-21 12:49:19 +0300

    Added url paths

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('home/', views.home, name='home'),
+    path('hood/new/', views.add_hood, name='create-hood'),
+    path('hood/<int:id>/', views.hood_detail, name='hood-detail'),
+    path('post/new/<int:hood_id>/', views.post, name='new-post'),
 ]
